a9e07156475dc391bd36b24cb8b9fa60da51285f
Angie Lal <angielal@gmail.com>
2013-11-23 12:55:01 -0800

    Added hue, saturation, and brightness ivars and updateColor method.

--- ColorPicker/HBViewController.m
+++ ColorPicker/HBViewController.m
@@ -9,8 +9,13 @@
 #import "HBViewController.h"
 
 @interface HBViewController ()
+{
+    CGFloat _hue;
+    CGFloat _saturation;
+    CGFloat _brightness;
+}
 @property (weak, nonatomic) IBOutlet UIView *colorWell;
- 
+
 @end
 
 @implementation HBViewController
@@ -25,6 +30,28 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)updateColor
+{
+    UIColor *newColor = [UIColor colorWithHue:_hue saturation:_saturation brightness:_brightness alpha:1.0];
+    self.colorWell.backgroundColor = newColor;
+}
+- (IBAction)handleHueSlider:(UISlider *)sender {
+    _hue = sender.value;
+    [self updateColor];
+}
+
+- (IBAction)handleSaturationSlider:(UISlider *)sender {
+    _saturation = sender.value;
+    [self updateColor];
+
+}
+
+- (IBAction)handleBrightnessSlider:(UISlider *)sender {
+    _brightness = sender.value;
+    [self updateColor];
+
 }
 
 @end
