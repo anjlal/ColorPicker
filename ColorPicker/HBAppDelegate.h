3d729ba5fbd40d7b8b9c2f96a4df5125744c9a54
Angie Lal <angielal@gmail.com>
2013-11-23 12:14:04 -0800

    Initial Commit

---
+++ ColorPicker/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  ColorPicker
+//
+//  Created by Angie Lal on 11/23/13.
+//  Copyright (c) 2013 Angie Lal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorPicker/HBViewController.h
@@ -0,0 +1,13 @@
+//
+//  HBViewController.h
+//  ColorPicker
+//
+//  Created by Angie Lal on 11/23/13.
+//  Copyright (c) 2013 Angie Lal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBViewController : UIViewController
+
+@end
